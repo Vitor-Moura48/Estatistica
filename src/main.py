0e7cb13161a01c783d236405c96c2fe9fa1c8b24
Vitor Gabriel <gabriel.4815.7@gmail.com>
2024-05-22 16:44:00 -0300

    feat: Estrutura Inicial

---
+++ src/main.py
@@ -0,0 +1,7 @@
+import sys, os, matplotlib, numpy, pandas
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) # adiciona as pastas fora de src no caminho python
+import recursos
+
+if __name__ == "__main__":
+    pass
