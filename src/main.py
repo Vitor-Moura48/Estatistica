576535f652143f1291103048d5d595ceecbf6063
Vitor Gabriel <gabriel.4815.7@gmail.com>
2024-05-26 11:11:11 -0300

    feat: desvio padrão, histograma de idades

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
-import sys, os, matplotlib, numpy, pandas
+import sys, os, numpy, pandas
+import matplotlib.pyplot as plt
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) # adiciona as pastas fora de src no caminho python
-import recursos
 
-# média dos que sobreviveram V
-# mediana da idade dos que sobreviveram
-# moda da idade dos que sobreviveram
-# desvio padrão
+# média dos que sobreviveram (V)
+# mediana da idade dos que sobreviveram (V)
+# moda da idade dos que sobreviveram (V)
+# desvio padrão (V)
 # gráfico
-# dristriuição normal
+# distribuição normal
 
 if __name__ == "__main__":
     df = pandas.read_csv("recursos/titanic.csv")
@@ -24,6 +24,9 @@ if __name__ == "__main__":
 
     moda_vivos = round(vivos["Age"].mode().values[0])
     moda_mortos = round(mortos["Age"].mode().values[0])
+    
+    desvio_padrao_idade_vivos = vivos['Age'].std().round(3)
+    desvio_padrao_idade_mortos = mortos['Age'].std().round(3)
 
     print(media_vivos)
     print(media_mortos)
@@ -31,3 +34,9 @@ if __name__ == "__main__":
     print(mediana_mortos)
     print(moda_vivos)
     print(moda_mortos)
+    print(desvio_padrao_idade_vivos)
+    print(desvio_padrao_idade_mortos)
+
+    i = df.sort_values(by='Age')
+    plt.hist(i['Age'], bins=8, label='Histograma Idades')
+    plt.show()
